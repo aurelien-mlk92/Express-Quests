260af676a35bda3b94fcaa9d70efcb69a4cb88bc
aurelien.mlk92 <aurelien.geninsimonet@gmail.com>
2023-12-07 15:42:02 +0100

    post quest

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.APP_PORT;
+app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 
@@ -9,6 +10,7 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUsers);
 app.get("/api/users/:id", movieControllers.getUserById);
-
+app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/users", movieControllers.postUser)
 
 module.exports = app;
